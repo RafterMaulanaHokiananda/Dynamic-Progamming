0b89e8c4c86736125fc8a2730cadf96ac5cda74d
Rafter PC <Rafter PC@DESKTOP-221RD52>
2018-09-13 13:29:42 +0700

    Initial commit

---
+++ src/Dynamicprogamming/Latihan01.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dynamicprogamming;
+import java.math.BigInteger;
+import java.util.Scanner;
+/**
+ *
+ * @author Rafter PC
+ */
+public class Latihan01 {    
+    private static void tampilJudul(String identitas )
+    {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonaci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+                
+    }
+    
+    public static void main(String[] args) 
+    {
+         String identitas = " RAFTER MAULANA HOKIANANDA / XR2 / 27";
+         
+         tampilJudul(identitas);
+    }
+   
+      
+   
+    
+       
+}
