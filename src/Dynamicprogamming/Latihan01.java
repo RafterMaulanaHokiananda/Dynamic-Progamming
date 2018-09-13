27950c514b82fd6a27b1a894a32745abedb51b68
Rafter PC <Rafter PC@DESKTOP-221RD52>
2018-09-13 13:32:38 +0700

    Tambah Hitung Fibonacci

--- src/Dynamicprogamming/Latihan01.java
+++ src/Dynamicprogamming/Latihan01.java
@@ -42,5 +42,22 @@ public class Latihan01 {
     
     int n= tampilInput();
     
+    private static BigInteger fibo(int n)
+    {
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for(int i = 2; i < n; i++)
+        {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        
+        return hasil[n-1];
+    }
+    
+    BigInteger hasil = fibo(n);
+    
    
 }
