719b7f407cc4905bbb19f8a5f7979a4856388c76
Rafter PC <Rafter PC@DESKTOP-221RD52>
2018-09-14 19:32:22 +0700

    Array

---
+++ src/Dynamicprogamming/Tugas01.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dynamicprogamming;
+import java.util.Scanner;
+/**
+ * @author Rafter PC
+ */
+public class Tugas01 {
+    public static void main(String[] args) 
+    {
+        int[] nominal = {5000,2000,1000,500,100};
+        Scanner key = new Scanner(System.in);
+        
+    }
+}
